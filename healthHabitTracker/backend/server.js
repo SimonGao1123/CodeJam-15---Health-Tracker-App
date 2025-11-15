fc1c3d62bc6ea67b8eead02ec12abc504f12bc24
Simon Gao <simongao398@gmail.com>
2025-11-14 23:51:37 -0500

    troubleshooting for gemini backend

--- healthHabitTracker/backend/server.js
+++ healthHabitTracker/backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import loginRoutes from './routes/loginRoutes.js';
+import geminiRoutes from './routes/geminiRoute.js';
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/", loginRoutes);
+app.use("/", geminiRoutes);
+
 
 app.listen(3000, () => console.log("Server has started"));
 
